e7234ad8ff00573d0b02e76e823d008c9963a07e
Aishwarya <Aishwarya.lodhi@sjsu.edu>
2023-04-01 10:34:23 -0700

    Serviceapp

---
+++ app/build/generated/source/buildConfig/debug/sjsu/cmpe277/servicesapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sjsu.cmpe277.servicesapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "sjsu.cmpe277.servicesapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
